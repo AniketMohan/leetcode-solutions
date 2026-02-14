9a3dd24e1f046bb8fab9a96846f55666df77bfc1
Aniket Mohan <aniketmohan2828@gmail.com>
2026-02-14 23:09:40 +0530

    Refined and Modified 149.py

--- 149.py
+++ 149.py
@@ -8,36 +8,44 @@
 # -104 <= xi, yi <= 104
 # All the points are unique.
 
-# 2026-02-14 15:12 edit: time limit exceeded, slowwww
+import math
+
 def maxPoints(points) -> int: # points: list[list[int]]
-    if len(points) == 2 or len(points) == 1:
+    if len(points) in [1,2]:
         return len(points)
+
     lines = []
-    lines_vert = []
-    max_num_points_on_line = 0
-    for i in range(len(points)):
-        for j in range(i + 1, len(points)):
+    answer = 0
+    i = 0
+    while i < len(points) - 1:
+        j = i + 1
+        while j < len(points):
             x1 = points[i][0]
             y1 = points[i][1]
             x2 = points[j][0]
             y2 = points[j][1]
-
-            if x2 - x1 == 0:
-                if x1 in lines_vert:
-                    continue
-                lines_vert.append(x1)
-                max_num_points_on_line = max(max_num_points_on_line, len(list(filter(lambda point: point[0] == x1, points))))
-            else:
-                c = x2 - x1
-                m = (y2 - y1)/c
-                b = y1 - m * x1
-
-                if [m, b] in lines:
-                    continue
-                lines.append([m,b])
-                max_num_points_on_line = max(max_num_points_on_line, len(list(filter(lambda point: c * point[1] == (y2 - y1) * point[0] + b * c, points))))
-    return max_num_points_on_line
+            a = y2 - y1
+            b = x1 - x2
+            c = x2 * y1 - x1 * y2
+            normalizing_factor = math.gcd(a,b,c)
+            if a:
+                if a < 0:
+                    normalizing_factor *= -1
+            elif b:
+                if b < 0:
+                    normalizing_factor *= -1
+            line = [
+                a//normalizing_factor,
+                b//normalizing_factor,
+                c//normalizing_factor
+            ]
+            if line not in lines:
+                lines.append(line)
+                answer = max(answer,len(list(filter(lambda point: a * point[0] + b * point[1] + c == 0,points))))
+            j += 1
+        i += 1
+    return answer
 
 # Testing
-for points in [[[-6,-1],[3,1],[12,3]]]: #[[-2, -2], [-1,-1], [1,1],[2,2],[3,3], [1,4], [1,5], [4,1], [4,2], [4,3], [4,4]], [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]], 
+for points in [[[-6,-1],[3,1],[12,3]], [[-2, -2], [-1,-1], [1,1],[2,2],[3,3], [1,4], [1,5], [4,1], [4,2], [4,3], [4,4]], [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]]: 
     print(points,maxPoints(points))
